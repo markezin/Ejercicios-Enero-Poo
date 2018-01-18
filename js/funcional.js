5d2480df56492a205172096fc573181a25490b15
Unknown <carlosmarquezlorenzo@hotmail.com>
2018-01-18 14:48:23 +0100

    ejercicio 1 y 2 correctos

--- js/funcional.js
+++ js/funcional.js
@@ -9,8 +9,8 @@ function factorial (numero) {
 
 			else{
 
-		return factorial(numero-1)*numero;
-}
+		return factorial (numero-1)*numero;
+	}
 }
 
 console.log(factorial(5))
@@ -18,14 +18,47 @@ console.log(factorial(5))
 
 
 
-function gritar (numero, letra) {
+// function gritar (numero, letra) {
+// 	if ( num == 0 ) {
+//         return 0;	
 
-	if(numero===1)
-		return letra+letra+"!";
+// 	if(numero==1)
+// 		return letra+letra+"!";
+// 		// return "!"
+// 	else{
+// 		// return(numero-1,)
+// 	}
+// }
 
-	else{
-		return letra+(gritar(numero-1, letra));
-	}
-}
+// console.log (gritar(3,"A"))
 
-console.log (gritar(3,"A"))
+
+// function gritar( num, vocal ) {
+//     if ( num == 0 ) {
+//         return 0;
+//     }
+//     else if (num == 1 )
+//         return vocal + vocal;
+//     else {
+//         return  vocal + gritar(num - 1, vocal);
+//     }
+// }
+// console.log(gritar(0, "a"));
+
+
+        /*
+            2) funcion gritar (numero, letra)
+            Pinta por consola tantas veces seguidas como el doble del parámetro número
+        */
+        function gritar (num, letra) {
+            if (num == 0) {
+                return "!";
+            }
+/*            if (num == 1) {
+                return letra + "!";
+            }*/
+            if (num >= 1) {
+                return letra + letra + gritar(num-1, letra);
+            }
+        }
+console.log(gritar(4, "a"));
